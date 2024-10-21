8ab2b1979826bb51606ce2ff0231f0ee502de98a
agusm2 <AGUSTIN.MALDONADO@correo.ucu.edu.uy>
2024-10-21 09:03:41 -0300

    Agrego clases

--- src/Library/ConsolePrinter.cs
+++ src/Library/ConsolePrinter.cs
@@ -4,9 +4,9 @@ namespace Full_GRASP_And_SOLID
 {
     public class ConsolePrinter : IPrinter
     {
-        public void PrintRecipe(Recipe recipe)
+        public void PrintRecipe(IGetRecipe recipe)
         {
-            Console.WriteLine(recipe.GetTextToPrint());
+            Console.WriteLine(recipe.RecipeToPrint());
         }
     }
 }
---
+++ src/Library/IGetRecipe.cs
@@ -0,0 +1,6 @@
+namespace Full_GRASP_And_SOLID;
+
+public interface IGetRecipe
+{
+    string RecipeToPrint();
+}
